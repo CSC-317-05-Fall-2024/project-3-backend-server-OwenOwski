95e639a97baad74e067daeb28f8da63f50f13c60
Owen Rombawa <rombawao@gmail.com>
2024-10-23 19:21:49 -0700

    Got delete functionality

--- server/data/restaurants.js
+++ server/data/restaurants.js
@@ -1,9 +1,81 @@
 // Fill this in
-let restaurantData = [
+let restaurants = [
+    {   
+        id:0,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {   
+        id:1,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {
+        id:2,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {   
+        id:3,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {   
+        id:4,
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234",
+        image: "/images/marysol.jpg"
+    },
+    {   
+        id:5,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {
+        id:6,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {
+        id:7,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
+    {
+        id:8,
+        image: "/images/marysol.jpg",
+        name: "Restaurant Name",
+        add: "1234 Something Ave",
+        add1:"Los Angeles, CA 12345",
+        num: "(123) 123 - 1234"
+    },
 
 ];
 
-let lastId = restaurantData.length;
+let lastId = restaurants.length;
 
 const getNextId = () => {
     lastId += 1;
@@ -12,19 +84,34 @@ const getNextId = () => {
 
 // Get a list of restaurants
 const getRestaurants = () => {
+    return restaurants
 };
 
 
 // Get a restaurant by id
 const getRestaurant = (id) => {
+    return restaurants.find(restaurant => restaurant.id === id);
 };
 
 // Create a new restaurant entry
-const createRestaurant = (newRestaurant) => {
+const createRestaurant = (data) => {
+    const newRestaurant = {
+        id: lastId(),
+        ...data
+    }
+    restaurants.push(newRestaurant);
+    return newRestaurant;
 };
 
 // Delete a restaurant by id
 const deleteRestaurant = (id) => {
+    console.log(id);
+    const restaurantToDelete = restaurants.find(restaurant => restaurant.id === id);
+    if (!restaurantToDelete) {
+        throw Error(`restaurant ${id} not found!`);
+    }
+    restaurants = restaurants.filter(restaurant => restaurant.id !== id);
+    return restaurantToDelete;
 };
 
-export { getRestaurants, getRestaurant, createRestaurant, deleteRestaurant };
+export { getRestaurants, getRestaurant, createRestaurant, deleteRestaurant, restaurants};
